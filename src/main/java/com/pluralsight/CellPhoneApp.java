a2e209f99c1f5dd5738fa4f839bebec47610708f
Richie <richiesyin@icloud.com>
2025-10-01 16:40:15 -0700

    CellPhoneService

---
+++ src/main/java/com/pluralsight/CellPhoneApp.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CellPhone cell = new CellPhone();
+        System.out.println("What is the serial number?");
+        cell.setSerialNumber(Long.parseLong(scanner.nextLine()));
+        System.out.println("What model is the phone?");
+        cell.setModel(scanner.nextLine());
+        System.out.println("Who is the carrier?");
+        cell.setCarrier(scanner.nextLine());
+        System.out.println("What is the phone number?");
+        cell.setPhoneNumber(scanner.nextLine());
+        System.out.println("Who is the owner of the phone?");
+        cell.setOwner(scanner.nextLine());
+
+        System.out.println("serial number: " + cell.getSerialNumber());
+        System.out.println("model: " + cell.getModel());
+        System.out.println("carrier: " + cell.getCarrier());
+        System.out.println("phone number: " + cell.getPhoneNumber());
+        System.out.println("owner: " + cell.getOwner());
+//        int index = 0;
+//        while(cell <= index) {
+//            System.out.println(cell);
+//            index++;
+//        }
+    }
+
+
+}
